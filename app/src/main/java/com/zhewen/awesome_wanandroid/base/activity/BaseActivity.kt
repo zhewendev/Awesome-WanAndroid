a60874a04d27d95621b9fbf705e07d1194d566b5
ruanwen <11085273@bbktel.com>
2020-05-12 21:56:26 +0800

    [D200512]补充Awesome-WanAndroid工程

---
+++ app/src/main/java/com/zhewen/awesome_wanandroid/base/activity/BaseActivity.kt
@@ -0,0 +1,9 @@
+package com.zhewen.awesome_wanandroid.base.activity
+
+/**
+* @author: zhewen
+* created: 2020/5/12
+* desc:BaseActivity 基类
+*/
+abstract class BaseActivity {
+}
---
+++ app/src/main/java/com/zhewen/awesome_wanandroid/presenter/AbstractPresenter.kt
@@ -0,0 +1,9 @@
+package com.zhewen.awesome_wanandroid.presenter
+
+/**
+* @author: zhewen
+* created: 2020/5/12
+* desc:presenter 基类
+*/
+interface AbstractPresenter {
+}
